138233253510dca0f433322942bc0be9acbc4d38
BALDE Thierno Lamine <tlbbalde92@gmail.com>
2017-07-17 18:37:20 +0000

    add redister on admin.py

--- admission/admin.py
+++ admission/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-# from  .models import Saisie
-# from  .models import Inscription
+from  .models import Saisie
+from  .models import Inscription
 
-# admin.site.register(Saisie)
-# admin.site.register(Inscription)
+admin.site.register(Saisie)
+admin.site.register(Inscription)
